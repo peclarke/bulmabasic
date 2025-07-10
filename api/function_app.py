5a1c49ba9a31baf45108b3d0e7d45f52e082691f
Paul Clarke <paulioclarke@icloud.com>
2025-07-10 11:54:59 +1000

    praying

---
+++ api/function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="info")
+def info(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse("Here's the info you asked for", status_code=200)
